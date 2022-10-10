b29e8244b0ebb656a1fd12a7633082ee504fb95b
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-10-10 11:58:31 -0300

    :bulb: Finished project

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,94 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './index.css'
+import axios from 'axios'
 import {
   UilWifi,
   UilLocationPoint,
-  UilMapPinAlt,
+  UilSearch,
   UilClockEight,
   UilMoneyBill
 } from '@iconscout/react-unicons'
-import ReactCountryFlag from "react-country-flag"
 
 import { MapContainer, TileLayer, useMap } from 'react-leaflet'
 
 
 
+
 function App() {
+  const [ip, setIp] = useState<any>()
+  const [data, setData] = useState<any>({})
+  const [error, setError] = useState<boolean>(false)
+
+  async function handleSubmit(e: any=false, userIp: any=false) {
+    await axios.get('https://api.techniknews.net/ipgeo/' + (userIp || ip))
+    .then((response) => {
+      setData(response.data)
+      setIp(response.data.ip)
+      if (response.data.status === 'fail')
+        setError(true)
+      else
+        setError(false)
+    })
+  }
+
+  function getLocation() {
+    if (data.city)
+      return `${data.city}, ${data.regionName} - ${data.country} (${data.continent})`
+    else
+      return ''
+  }
+
+  function getValue(variable: string){
+    return (data[variable]? data[variable] : '')
+  }
+
+  function getMaps(){
+    if (error) return ''
+    return `https://www.google.com/maps/embed/v1/place?key=AIzaSyAGf4lRqghBFSHhG70jJLmlhUh61MkEqEI&q=${data.lat},%20%20${data.lon}`
+  }
+
+  useEffect(() => {
+    const requestUserIp = async () => {
+      const event = false;
+      const userIP = ' ';
+      await handleSubmit(event, userIP)
+    }
+    requestUserIp()
+  }, [])
+
   return (
-    <div className='flex flex-col max-w-lg mx-auto mt-2 color-third px-6 mb-5'>
+    <div className='flex flex-col max-w-lg mx-auto mt-2 px-6 mb-5'>
       <div className='row my-10 text-center'>
         <h1 className='text-4xl text-center font-black color-complementary'>
           IP Location
         </h1>
-        <small className='text-center'>
+        <small className='text-center color-third font-medium'>
           Find someone by their IP.
         </small>
       </div>
 
       <div className='row mt-[30px]'>
+        <div
+
+        className={!error ? 'hidden' : 'h-10 bg-[#FF0000] rounded flex items-center justify-center mb-3'}>
+          <h3>
+            <strong>Ops!</strong> The inserted IP invalid.
+          </h3>
+        </div>
         <div className='input-group'>
           <div className='input-icon-group rounded'>
             <UilWifi className='input-icon' size='18' color="#FFECEF"/>
           </div>
+          <button
+          type='submit'
+          onClick={(e) => {handleSubmit(e)}}
+          className='search-icon-group rounded hover:bg-[#634c80]'>
+            <UilSearch className='search-icon' size='18' color="#FFECEF"/>
+          </button>
           <input
-            disabled
-            defaultValue={'192.168.0.1'}
+            autoFocus
+            value={ip}
+            onChange={(e) => setIp(e.target.value)}
             className='w-full rounded p-1 bg-fourth' type='text'/>
         </div>
       </div>
@@ -44,7 +100,7 @@ function App() {
           </div>
           <input
             disabled
-            defaultValue={'Brazil - Brasília, 72603-717'}
+            value={getLocation()}
             className='w-full rounded p-1 bg-fourth' type='text'/>
         </div>
       </div>
@@ -58,7 +114,7 @@ function App() {
           </div>
           <input
             disabled
-            defaultValue={'-15.7792'}
+            value={getValue('lat')}
             className='w-full rounded p-1 bg-fourth' type='text'/>
         </div>
 
@@ -70,7 +126,7 @@ function App() {
           </div>
           <input
             disabled
-            defaultValue={'-47.9341'}
+            value={getValue('lon')}
             className='w-full rounded p-1 bg-fourth' type='text'/>
         </div>
       </div>
@@ -82,7 +138,7 @@ function App() {
           </div>
           <input
             disabled
-            defaultValue={'America/Sao_Paulo'}
+            value={getValue('timezone')}
             className='w-full rounded p-1 bg-fourth' type='text'/>
         </div>
       </div>
@@ -94,7 +150,7 @@ function App() {
           </div>
           <input
             disabled
-            defaultValue={'BRL'}
+            defaultValue={getValue('currency')}
             className='w-full rounded p-1 bg-fourth' type='text'/>
         </div>
       </div>
@@ -104,8 +160,8 @@ function App() {
             <iframe
             className='rounded'
             scrolling="no"
-            src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=-15.7792,%20%20-47.9341+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">
-              distance maps</a>
+            loading='lazy'
+            src={getMaps()}>
             </iframe>
           </div>
       </div>
